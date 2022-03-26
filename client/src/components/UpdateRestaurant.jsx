528dc47b074cca65145631f05c77e9d637c5843d
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-03-26 16:45:56 -0400

    add update component

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,31 +1,86 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useContext, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import RestaurantsFinder from "../api/RestaurantsFinder";
 
 const UpdateRestaurant = (props) => {
-  const test = useParams();
-  console.log(test);
+  const { id } = useParams();
+  let navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [priceRange, setPriceRange] = useState("");
 
-  return(
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantsFinder.get(`/${id}`);
+        
+        setName(response.data.restaurant[0].name);
+        setLocation(response.data.restaurant[0].location);
+        setPriceRange(response.data.restaurant[0].price_range);
+      } catch (err) {
+        console.log(err)
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const updatedRestaurant = await RestaurantsFinder.put(`/${id}`, {
+      name,
+      location,
+      price_range: priceRange,
+    });
+    navigate("/");
+  };
+
+  return (
     <div>
-      <div className="col">
-        <input  className="form-control" type='text' placeholder="name" />
-      </div>
-      <div className="col">
-        <input  className="form-control" type='text' placeholder="location" />
-      </div>
-      <div className="col">
-        <select className="custom-select my-1 mr-sm-2">
-          <option disabled>Price Range</option>
-          <option value='1'>$</option>
-          <option value='2'>$$</option>
-          <option value='3'>$$$</option>
-          <option value='4'>$$$$</option>
-          <option value='5'>$$$$$</option>
-        </select>
-      </div>
-      <button type="submit" className="btn btn-primary">Update</button>
+      <form action="">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            id="name"
+            className="form-control"
+            type="text"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="location">Location</label>
+          <input
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            className="form-control"
+            type="text"
+          />
+        </div>
+
+        <div className="col">
+        <select value={priceRange} onChange={e => setPriceRange(e.target.value)} className="custom-select my-1 mr-sm-2">
+              <option disabled>Price Range</option>
+              <option value='1'>$</option>
+              <option value='2'>$$</option>
+              <option value='3'>$$$</option>
+              <option value='4'>$$$$</option>
+              <option value='5'>$$$$$</option>
+            </select>
+        </div>
+
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="btn btn-primary"
+        >
+          Update
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default UpdateRestaurant;
