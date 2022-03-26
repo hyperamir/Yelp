97f711bc416d6668f694e6b29c85132dc5788453
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-03-26 09:31:49 -0400

    fetch restaurants list from backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import UpdatePage from './routes/UpdatePage';
+import RestaurantDetailPage from './routes/RestaurantDetailPage';
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
 
 function App() {
   return (
-    <div className="App">
-      test
-    </div>
+    <RestaurantsContextProvider>
+      <div className='container'>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/restaurants/:id/update' element={<UpdatePage />} />
+            <Route path='/restaurants/:id' element={<RestaurantDetailPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
   );
 }
 
